47d3f0dddf66c4a529dc57fe4619b82142656a73
JulianOrjuela <u1202021@unimilitar.edu.co>
2018-07-26 16:09:25 -0500

    Update_Kata
    Update of the online kata problem.

--- bogota-july-2018/cyclomatic-complexity/JulianOrjuelaPelaez/Kata.js
+++ bogota-july-2018/cyclomatic-complexity/JulianOrjuelaPelaez/Kata.js
@@ -7,13 +7,8 @@ function numSum(total,actualNumber){
   return total + actualNumber;
 }
 
-function divide(numerator,denominator){
-  return numerator/denominator;
-}
-
-function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
-  const ageArray = [age1,age2,age3,age4,age5,age6,age7,age8];
-  let raisedArray = ageArray.map(numRaisedBy);
+function predictAge(...age){
+  let raisedArray = age.map(numRaisedBy);
   let Age = raisedArray.reduce(numSum);
   return Math.floor(Math.sqrt(Age)/2)
 }
